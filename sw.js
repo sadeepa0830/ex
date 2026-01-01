efda15c8a212f1ec5e8b98ce2f6ade7b9eeb828e
sadeepa0830 <sadeepalakshan663@gmail.com>
2026-01-01 09:15:00 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'exam-master-v3';
-const urlsToCache = ['/','/index.html','/style.css','/app.js','/manifest.json','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME ? caches.delete(k) : null)))));
